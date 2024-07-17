19fc8d9b483f92e59600c15809cb7937e1b21617
PKCubed <59457514+PKCubed@users.noreply.github.com>
2024-07-17 16:30:29 -0700

    Create __init__.py

---
+++ ProPresenter/__init__.py
@@ -0,0 +1,5 @@
+"""
+Peter's ProPresenter Integration
+
+This integration links to ProPressenter in order to control things such as lights and systems from the current slide presented from ProPresenter.
+"""
