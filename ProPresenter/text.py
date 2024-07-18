9e8dab8f0bfa312c4b77a56d59e517d75980cf90
PKCubed <59457514+PKCubed@users.noreply.github.com>
2024-07-17 21:23:38 -0700

    Update text.py

--- ProPresenter/text.py
+++ ProPresenter/text.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from homeassistant.components.text import (
-    TextEntity
-)
+from homeassistant.components.text import TextEntity
+from homeassistant.components.number import NumberEntity
 from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -21,18 +20,18 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     """Set up the sensor platform."""
-    add_entities([ProPresenter()])
+    add_entities([Slide_Notes()])
 
 
-class ProPresenter(TextEntity):
-    """Representation of a Sensor."""
+class Slide_Notes(TextEntity):
+    """Representation of a text_input."""
 
     def __init__(self):
         self._attr_name = "Slide Notes"
-        self._attr_unique_id = "sensor.propresenter_slide_notes"
-        self.entity_id = "sensor.propresenter_slide_notes"
-        self.last_time = time.time()
+        self._attr_unique_id = "text.propresenter_slide_notes"
+        self.entity_id = "text.propresenter_slide_notes"
         self._attr_native_value = ""
+        self._attr_device_class = "ProPresenter"
 
     def update(self) -> None:
         """
@@ -41,7 +40,7 @@ class ProPresenter(TextEntity):
         """
         try:
             response = requests.get(api_url+"v1/status/slide",timeout=0.01)
-            self._attr_native_value = response.json()['current']['notes']
+            self._attr_native_value = str(response.json()['current']['notes'])
         except:
             pass
             
